241336f10f57969be2e43c37fec372f205bd6b32
lucasbclarke <lucasbaileyclarke@outlook.com>
2025-05-19 21:42:13 +1000

    fixed mason-lspconfig issue, only on some systems does the issue come up?

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,25 @@
-require('mason').setup({})
-require('mason-lspconfig').setup({
-    ensure_installed = {'lua_ls', 'zls'},
+-- mason.lua
+require("mason").setup()
+
+require("mason-lspconfig").setup({
+    ensure_installed = { "lua_ls", "zls" },
+    automatic_installation = false,   -- explicitly disable
+    automatic_enable = false,    
 })
-require('lspconfig').zls.setup{}
+
+local lspconfig = require("lspconfig")
+
+-- Setup for ZLS (Zig LSP)
+lspconfig.zls.setup({})
+
+-- Setup for Lua LS (recommended extras for neovim development)
+lspconfig.lua_ls.setup({
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { "vim" },
+            },
+        },
+    },
+})
+
